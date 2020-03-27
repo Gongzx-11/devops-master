0a81c2f68a0d23688c1fc4e04354f1455fcbd932
Gongzx <2223407981@qq.com>
2020-03-27 14:11:02 +0800

    新增提示，更改Tomcat启动方式，修复Tomcat日志不输出问题

--- modules/agent/src/main/java/gzx/devops/common/commander/impl/WindowsTomcatCommander.java
+++ modules/agent/src/main/java/gzx/devops/common/commander/impl/WindowsTomcatCommander.java
@@ -4,6 +4,8 @@ import cn.hutool.core.util.StrUtil;
 import gzx.devops.common.commander.AbstractTomcatCommander;
 import gzx.devops.model.data.TomcatInfoModel;
 
+import javax.xml.soap.SAAJResult;
+
 /**
  * tomcat的Windows管理命令
  *
@@ -21,24 +23,38 @@ public class WindowsTomcatCommander extends AbstractTomcatCommander {
     @Override
     public String execCmd(TomcatInfoModel tomcatInfoModel, String cmd) {
         String tomcatPath = tomcatInfoModel.pathAndCheck();
+        //截取盘符
+        String dcPath = null;
+        if (tomcatPath != null && tomcatPath.indexOf("/") > 1) {
+            dcPath = tomcatPath.substring(0,tomcatPath.indexOf("/"));
+        }
+
+        String command = null;
         if (StrUtil.isBlank(tomcatPath)) {
             return "tomcat path blank";
         }
-        // 拼接命令
-        String command = String.format("cmd /c java -Djava.util.logging.config.file=\"%sconf/logging.properties\" " +
-                        "-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager  " +
-                        "-Djdk.tls.ephemeralDHKeySize=2048" +
-                        "-Djava.protocol.handler.pkgs=org.apache.catalina.webresources   " +
-                        "-Dignore.endorsed.dirs=\"%s\" " +
-                        "-classpath \"%sbin/bootstrap.jar;%sbin/tomcat-juli.jar\" " +
-                        "-Dcatalina.base=\"%s\" " +
-                        "-Dcatalina.home=\"%s\" " +
-                        "-Djava.io.tmpdir=\"%stemp/\" " +
-                        "org.apache.catalina.startup.Bootstrap %s", tomcatPath, tomcatPath,
-                tomcatPath, tomcatPath, tomcatPath,
-                tomcatPath, tomcatPath, cmd);
-        //
-        exec(command, true);
+        if (cmd.equals("stop")) {
+            // 拼接命令
+            command = String.format("cmd /c java -Djava.util.logging.config.file=\"%sconf/logging.properties\" " +
+                            "-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager  " +
+                            "-Djdk.tls.ephemeralDHKeySize=2048" +
+                            "-Djava.protocol.handler.pkgs=org.apache.catalina.webresources   " +
+                            "-Dignore.endorsed.dirs=\"%s\" " +
+                            "-classpath \"%sbin/bootstrap.jar;%sbin/tomcat-juli.jar\" " +
+                            "-Dcatalina.base=\"%s\" " +
+                            "-Dcatalina.home=\"%s\" " +
+                            "-Djava.io.tmpdir=\"%stemp/\" " +
+                            "org.apache.catalina.startup.Bootstrap %s", tomcatPath, tomcatPath,
+                    tomcatPath, tomcatPath, tomcatPath,
+                    tomcatPath, tomcatPath, cmd);
+            //
+            exec(command, true);
+        } else {
+//            command = String.format("cmd.exe /K start /b %sbin/startup.bat", tomcatPath);
+            command = String.format("cmd /k %s && cd %s/bin && start startup.bat", dcPath, tomcatPath);
+            exec(command, true);
+        }
+
         // 查询操作结果并返回
         return getStatus(tomcatInfoModel, cmd);
     }
